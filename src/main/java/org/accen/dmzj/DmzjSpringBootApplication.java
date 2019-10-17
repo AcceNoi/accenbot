fc02c7ffcf704cffe2708d91b7608cc3c83e314f
liuw <liuw@ebidding.net.cn>
2019-10-17 22:37:52 +0800

    放开定时任务

--- src/main/java/org/accen/dmzj/DmzjSpringBootApplication.java
+++ src/main/java/org/accen/dmzj/DmzjSpringBootApplication.java
@@ -4,7 +4,9 @@ import org.accen.dmzj.util.ApplicationContextUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
+@EnableScheduling
 public class DmzjSpringBootApplication {
 
 	public static void main(String[] args) {
