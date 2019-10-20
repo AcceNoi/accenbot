87ce323b33f799cc2e1dcd85769ba137298afcf0
liuw <liuw@ebidding.net.cn>
2019-10-20 17:12:37 +0800

    setu功能增加限制，降低访问频率

--- src/main/java/org/accen/dmzj/core/handler/cmd/SetuCmd.java
+++ src/main/java/org/accen/dmzj/core/handler/cmd/SetuCmd.java
@@ -27,26 +27,46 @@ public class SetuCmd implements CmdAdapter {
 
 	private static final Pattern pattern = Pattern.compile("^随机(色图|瑟图|涩图)$");
 	
+	private Boolean locked = false;//未知原因使得此功能被滥用则回系统崩溃，可能是coolq pro接收数据的超时设置问题，这里为了防止滥用，同一时间段只接收一个请求。
+	
 	@Override
 	public GeneralTask cmdAdapt(Qmessage qmessage, String selfQnum) {
-		String message = qmessage.getMessage().trim();
-		Matcher matcher = pattern.matcher(message);
-		if(matcher.matches()) {
-			String imageUrl = loliconApiClientPk.setu();
-			if(imageUrl!=null) {
-				GeneralTask task =  new GeneralTask();
+		if(locked) {
+			return null;
+		}else {
+			synchronized (locked) {
 				
-				task.setSelfQnum(selfQnum);
-				task.setType(qmessage.getMessageType());
-				task.setTargetId(qmessage.getGroupId());
-				task.setMessage(CQUtil.imageUrl(imageUrl));
-				return task;
+				if(locked) {
+					return null;
+				}else {
+					locked = true;
+					String message = qmessage.getMessage().trim();
+					Matcher matcher = pattern.matcher(message);
+					if(matcher.matches()) {
+						String imageUrl = loliconApiClientPk.setu();
+						if(imageUrl!=null) {
+							GeneralTask task =  new GeneralTask();
+							
+							task.setSelfQnum(selfQnum);
+							task.setType(qmessage.getMessageType());
+							task.setTargetId(qmessage.getGroupId());
+							task.setMessage(CQUtil.imageUrl(imageUrl));
+							
+							locked = false;
+							return task;
+						}
+						
+						
+						
+					}
+					locked = false;
+					return null;
+				}
+				
 			}
-			
-			
-			
 		}
-		return null;
+		
+		
 	}
 
 }
