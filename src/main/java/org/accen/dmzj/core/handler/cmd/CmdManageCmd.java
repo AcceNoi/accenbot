7a7bd9b0a5700b419f6a4de940474c1ebfc68e70
Accen <1339liu@gmail.com>
2019-10-28 09:41:35 +0800

    功能

--- src/main/java/org/accen/dmzj/core/handler/cmd/CmdManageCmd.java
+++ src/main/java/org/accen/dmzj/core/handler/cmd/CmdManageCmd.java
@@ -26,8 +26,8 @@ public class CmdManageCmd implements CmdAdapter {
 
 	@Override
 	public GeneralTask cmdAdapt(Qmessage qmessage, String selfQnum) {
-		
-		initCmds();
+		//直接做个简单的
+//		initCmds();
 		
 		String message = qmessage.getMessage().trim();
 		if("功能".equals(message)) {
@@ -35,10 +35,23 @@ public class CmdManageCmd implements CmdAdapter {
 			task.setSelfQnum(selfQnum);
 			task.setType(qmessage.getMessageType());
 			task.setTargetId(qmessage.getGroupId());
+			StringBuffer func = new StringBuffer();
+			func.append("1. 搜图 发送【老婆找图+[图片]】 \n")
+				.append("2. 翻译 发送【日语|英语|俄语|法语|西班牙语说+[要翻译的内容]】\n")
+				.append("3. 百科 发送【了解+[要百科的内容]】\n")
+				.append("4. 随机图片 发送【随机瑟图】\n")
+				.append("5. 点歌 发送【网易点歌+[歌名]】\n")
+				.append("6. 定时提醒 发送【老婆xx小时xx分钟xx秒后提醒我+[需要提醒的事]】\n")
+				.append("7. 抽卡 发送【影之诗抽卡+[对应的卡包]】\n")
+				.append("8. 复读 发送【老婆说+[想复读的内容]】\n")
+				.append("9. 语音复读 发送【说+[想复读的内容]】\n")
+				.append("10. 词条 发送【添加[精确]问xx答[回复]xx】");
 			task.setMessage(
-					cmds.stream()
-						.map(cmd->cmds.indexOf(cmd)+1+"."+cmd.describe()+"示例："+cmd.example())
-						.collect(Collectors.joining("\n")));
+//					cmds.stream()
+//						.map(cmd->cmds.indexOf(cmd)+1+"."+cmd.describe()+"示例："+cmd.example())
+//						.collect(Collectors.joining("\n"))
+					func.toString()
+						);
 			return task;
 		}
 		return null;
