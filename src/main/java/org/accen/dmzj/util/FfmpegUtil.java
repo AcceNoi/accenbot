231af1a18516487298e4de63be426e5d2547d756
Accen <1339liu@gmail.com>
2019-10-29 17:30:54 +0800

    新增silk v3的解码

--- src/main/java/org/accen/dmzj/util/FfmpegUtil.java
+++ src/main/java/org/accen/dmzj/util/FfmpegUtil.java
@@ -14,6 +14,8 @@ import org.springframework.stereotype.Component;
 public class FfmpegUtil {
 	@Value("${ffmpeg.bin}")
 	private String ffmpegBin ;
+	@Value("${silkv3.bin}")
+	private String silkBin;
 	
 	private static final Logger logger = LoggerFactory.getLogger(FfmpegUtil.class);
 	/**
@@ -104,5 +106,38 @@ public class FfmpegUtil {
 			return false;
 		}
 		return true;
+	}
+	
+	/**
+	 * qq录音中的silk格式转wav
+	 * @param silk
+	 * @return
+	 */
+	public String silk2Wav(String silk) {
+		String[] exe = new String[3];
+		if(SystemUtil.getOs().startsWith("LINUX")) {
+			String cmd= silkBin+"converter.sh "+silk+" wav";
+			logger.info("silk cmd: "+cmd);
+			exe[0] = "sh";
+			exe[1] = "-c";
+		}else if(SystemUtil.getOs().startsWith("WINDOWS")) {
+//			String cmd = silkBin+"silk_v3_decoder.exe "
+		}
+		Runtime run = Runtime.getRuntime();
+		Process p;
+		try {
+			p = run.exec(exe);
+			p.getOutputStream().close();
+			p.getInputStream().close();
+			p.getErrorStream().close();
+			p.waitFor();
+			return silk.substring(0, silk.lastIndexOf(".silk"))+".wav";
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return null;
+		
 	}
 }
