0d5167e78eac2aebfaeb879653f3dbeead4d165e
Accen <1339liu@gmail.com>
2019-11-05 19:52:31 +0800

    修复Grep功能同步的问题

--- src/main/java/org/accen/dmzj/core/handler/cmd/BiliBiliAudioGrepCmd.java
+++ src/main/java/org/accen/dmzj/core/handler/cmd/BiliBiliAudioGrepCmd.java
@@ -69,96 +69,98 @@ public class BiliBiliAudioGrepCmd implements CmdAdapter {
 	
 	private final static Pattern grepPattern = Pattern.compile("^抽取B站(.+)?从(.+)?到(.+)?(音乐|语音)，设置名称(.+)?$");
 	@Override
-	public synchronized GeneralTask cmdAdapt(Qmessage qmessage, String selfQnum) {
+	public  GeneralTask cmdAdapt(Qmessage qmessage, String selfQnum) {
 		String message = qmessage.getMessage().trim();
 		Matcher matcher = grepPattern.matcher(message);
 		if(matcher.matches()) {
-			
-			GeneralTask task = new GeneralTask();
-			task.setSelfQnum(selfQnum);
-			task.setType(qmessage.getMessageType());
-			task.setTargetId(qmessage.getGroupId());
-			
-			//金币检验
-			int curCoin = checkinCmd.getCoin(qmessage.getMessageType(), qmessage.getGroupId(), qmessage.getUserId());
-			if(curCoin<0) {
-				task.setMessage(CQUtil.at(qmessage.getUserId())+" 您还未绑定哦，暂时无法添加歌曲，发送[绑定]即可绑定个人信息喵~");
-				return task;
-			}else if(curCoin-coinDescrease<0) {
-				task.setMessage(CQUtil.at(qmessage.getUserId())+" 您库存金币不够了哦，暂无法添加词条喵~");
-				return task;
-			}else {
-				String url = matcher.group(1);
-				String ss = matcher.group(2);
-				String tt = matcher.group(3);
-				String type = matcher.group(4);
-				String name = matcher.group(5);
+			synchronized (this) {
+				GeneralTask task = new GeneralTask();
+				task.setSelfQnum(selfQnum);
+				task.setType(qmessage.getMessageType());
+				task.setTargetId(qmessage.getGroupId());
 				
-				int diff = ffmpegUtil.checkTimeIllegalEx(ss, tt);
-				if(diff<=0) {
-					task.setMessage("时间格式输入错误喵~");
+				//金币检验
+				int curCoin = checkinCmd.getCoin(qmessage.getMessageType(), qmessage.getGroupId(), qmessage.getUserId());
+				if(curCoin<0) {
+					task.setMessage(CQUtil.at(qmessage.getUserId())+" 您还未绑定哦，暂时无法添加歌曲，发送[绑定]即可绑定个人信息喵~");
 					return task;
-				}else if(diff>120&&"语音".equals(type)){
-					//如果大于120且为语音，则不给过
-					task.setMessage("抽取的语音时长不能超过120秒的说~");
+				}else if(curCoin-coinDescrease<0) {
+					task.setMessage(CQUtil.at(qmessage.getUserId())+" 您库存金币不够了哦，暂无法添加词条喵~");
 					return task;
-				}
-				try {
+				}else {
+					String url = matcher.group(1);
+					String ss = matcher.group(2);
+					String tt = matcher.group(3);
+					String type = matcher.group(4);
+					String name = matcher.group(5);
 					
-					taskManager.addGeneralTaskQuick(selfQnum, qmessage.getMessageType(), qmessage.getGroupId(), "视频["+url+"]解析中~");
-					
-					String[] rs = apiClient.downLoadAdaptive(url, 360);
-					String videoFile = rs[0];
-					//剪切音频
-					String targetFileName = StringUtil.uuid();
-					String target = tempMusicPath+KEY_PREFFIX+targetFileName+".aac";
-					String audio = ffmpegUtil.convertVideo2Audio(videoFile, target, "aac", ss, tt);
-					if(audio==null) {
-						task.setMessage("视频剪切失败喵~");
+					int diff = ffmpegUtil.checkTimeIllegalEx(ss, tt);
+					if(diff<=0) {
+						task.setMessage("时间格式输入错误喵~");
+						return task;
+					}else if(diff>120&&"语音".equals(type)){
+						//如果大于120且为语音，则不给过
+						task.setMessage("抽取的语音时长不能超过120秒的说~");
 						return task;
 					}
-					if("音乐".equals(type)) {
-						CfgResource cr = new CfgResource();
-						cr.setCfgKey(KEY_PREFFIX+name);
-						cr.setCfgResource(staticMusic+KEY_PREFFIX+targetFileName+".aac");
-						cr.setResourceType("music");
-						cr.setTitle(name);
-						cr.setContent(rs[2]);
-						cr.setImage(rs[1]);
-						cr.setOriginResource(rs[3]);
-						cr.setCreateUserId(qmessage.getUserId());
-						String createNickName = (String) ((Map<String, Object>)qmessage.getEvent().get("sender")).get("nickname");
-						String createCard = (String) ((Map<String, Object>)qmessage.getEvent().get("sender")).get("card");//群名片
-						cr.setCreateUserName(StringUtils.isEmpty(createCard)?createNickName:createCard);
-						cr.setCreateTime(new Date());
-						cfgResourceMapper.insert(cr);
+					try {
 						
-						//消耗金币
-						int newCoin = checkinCmd.modifyCoin(qmessage.getMessageType(), qmessage.getGroupId(), qmessage.getUserId(), -coinDescrease);
-						task.setMessage(CQUtil.at(qmessage.getUserId())+" 视频解析完成喵~触发词条为[B站点歌"+name+"]，本次消耗金币："+coinDescrease+"，剩余："+newCoin+"。Tips：每次点歌成功后会回赠1枚金币喵~");
-					}else {
-						CfgQuickReply reply = new CfgQuickReply();
-						reply.setMatchType(1);
-						reply.setPattern(StringUtil.transferPattern(name));
-						reply.setReply("[CQ:record,file="+staticMusic+KEY_PREFFIX+targetFileName+".aac,cache=0]");
-						reply.setApplyType(2);
-						reply.setApplyTarget(qmessage.getGroupId());
-						reply.setNeedAt(2);
-						reply.setCreateTime(new Date());
-						reply.setCreateUserId(qmessage.getUserId());
-						reply.setStatus(1);
-						cfgQuickReplyMapper.insert(reply);
-						//消耗金币
-						int newCoin = checkinCmd.modifyCoin(qmessage.getMessageType(), qmessage.getGroupId(), qmessage.getUserId(), -decrease);
-						task.setMessage(CQUtil.at(qmessage.getUserId())+" 视频解析完成喵~添加词条编号："+reply.getId()+"，本次消耗金币："+decrease+"，剩余："+newCoin);
+						taskManager.addGeneralTaskQuick(selfQnum, qmessage.getMessageType(), qmessage.getGroupId(), "视频["+url+"]解析中~");
+						
+						String[] rs = apiClient.downLoadAdaptive(url, 360);
+						String videoFile = rs[0];
+						//剪切音频
+						String targetFileName = StringUtil.uuid();
+						String target = tempMusicPath+KEY_PREFFIX+targetFileName+".aac";
+						String audio = ffmpegUtil.convertVideo2Audio(videoFile, target, "aac", ss, tt);
+						if(audio==null) {
+							task.setMessage("视频剪切失败喵~");
+							return task;
+						}
+						if("音乐".equals(type)) {
+							CfgResource cr = new CfgResource();
+							cr.setCfgKey(KEY_PREFFIX+name);
+							cr.setCfgResource(staticMusic+KEY_PREFFIX+targetFileName+".aac");
+							cr.setResourceType("music");
+							cr.setTitle(name);
+							cr.setContent(rs[2]);
+							cr.setImage(rs[1]);
+							cr.setOriginResource(rs[3]);
+							cr.setCreateUserId(qmessage.getUserId());
+							String createNickName = (String) ((Map<String, Object>)qmessage.getEvent().get("sender")).get("nickname");
+							String createCard = (String) ((Map<String, Object>)qmessage.getEvent().get("sender")).get("card");//群名片
+							cr.setCreateUserName(StringUtils.isEmpty(createCard)?createNickName:createCard);
+							cr.setCreateTime(new Date());
+							cfgResourceMapper.insert(cr);
+							
+							//消耗金币
+							int newCoin = checkinCmd.modifyCoin(qmessage.getMessageType(), qmessage.getGroupId(), qmessage.getUserId(), -coinDescrease);
+							task.setMessage(CQUtil.at(qmessage.getUserId())+" 视频解析完成喵~触发词条为[B站点歌"+name+"]，本次消耗金币："+coinDescrease+"，剩余："+newCoin+"。Tips：每次点歌成功后会回赠1枚金币喵~");
+						}else {
+							CfgQuickReply reply = new CfgQuickReply();
+							reply.setMatchType(1);
+							reply.setPattern(StringUtil.transferPattern(name));
+							reply.setReply("[CQ:record,file="+staticMusic+KEY_PREFFIX+targetFileName+".aac,cache=0]");
+							reply.setApplyType(2);
+							reply.setApplyTarget(qmessage.getGroupId());
+							reply.setNeedAt(2);
+							reply.setCreateTime(new Date());
+							reply.setCreateUserId(qmessage.getUserId());
+							reply.setStatus(1);
+							cfgQuickReplyMapper.insert(reply);
+							//消耗金币
+							int newCoin = checkinCmd.modifyCoin(qmessage.getMessageType(), qmessage.getGroupId(), qmessage.getUserId(), -decrease);
+							task.setMessage(CQUtil.at(qmessage.getUserId())+" 视频解析完成喵~添加词条编号："+reply.getId()+"，本次消耗金币："+decrease+"，剩余："+newCoin);
+						}
+						
+						
+						return task;
+					} catch (BiliBiliCookieNeverInit e) {
+						e.printStackTrace();
 					}
-					
-					
-					return task;
-				} catch (BiliBiliCookieNeverInit e) {
-					e.printStackTrace();
 				}
 			}
+			
 			
 			
 			
