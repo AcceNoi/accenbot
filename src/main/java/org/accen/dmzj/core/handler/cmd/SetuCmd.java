eb848ee5e2799dd3be7bc7aa182ecdcab54be6ea
Accen <1339liu@gmail.com>
2019-11-10 16:41:51 +0800

    新增setu的收藏功能

--- src/main/java/org/accen/dmzj/core/handler/cmd/SetuCmd.java
+++ src/main/java/org/accen/dmzj/core/handler/cmd/SetuCmd.java
@@ -1,19 +1,29 @@
 package org.accen.dmzj.core.handler.cmd;
 
+import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.accen.dmzj.core.annotation.FuncSwitch;
+import org.accen.dmzj.core.handler.callbacker.CallbackListener;
+import org.accen.dmzj.core.handler.callbacker.CallbackManager;
 import org.accen.dmzj.core.task.GeneralTask;
+import org.accen.dmzj.core.task.TaskManager;
 import org.accen.dmzj.core.task.api.LoliconApiClientPk;
+import org.accen.dmzj.core.timer.CacheMap;
 import org.accen.dmzj.util.CQUtil;
 import org.accen.dmzj.util.FuncSwitchUtil;
+import org.accen.dmzj.web.dao.CfgResourceMapper;
+import org.accen.dmzj.web.vo.CfgResource;
 import org.accen.dmzj.web.vo.Qmessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 @FuncSwitch("cmd_setu")
 @Component
-public class SetuCmd implements CmdAdapter {
+@Transactional
+public class SetuCmd implements CmdAdapter,CallbackListener {
 
 	@Override
 	public String describe() {
@@ -25,11 +35,22 @@ public class SetuCmd implements CmdAdapter {
 		return "随机涩图";
 	}
 	@Autowired
+	private CallbackManager callbackManager;
+	@Autowired
 	private LoliconApiClientPk loliconApiClientPk;
+	@Autowired
+	private CfgResourceMapper cfgResourceMapper;
 	@Autowired
 	private FuncSwitchUtil funcSwitchUtil;
 
+	@Autowired
+	private TaskManager taskManager;
+	
 	private static final Pattern pattern = Pattern.compile("^随机(色图|瑟图|涩图)$");
+	private static final Pattern collectPattern = Pattern.compile("^随机收藏$");
+	
+	
+	private CacheMap<String, String> waitingCollect = new CacheMap<String, String>();
 	
 //	private Boolean locked = false;//未知原因使得此功能被滥用则回系统崩溃，可能是coolq pro接收数据的超时设置问题，这里为了防止滥用，同一时间段只接收一个请求。
 	
@@ -56,12 +77,31 @@ public class SetuCmd implements CmdAdapter {
 							task.setTargetId(qmessage.getGroupId());
 							task.setMessage(CQUtil.imageUrl(imageUrl));
 							
+							//添加到收藏监听
+							waitingCollect.put(qmessage.getMessageType()+"_"+qmessage.getGroupId()+"_"+qmessage.getUserId(), imageUrl,15000);
+							callbackManager.addCallbackListener(this, qmessage);
+							
 //							locked = false;
 							return task;
 						}
 						
 						
 						
+					}
+					Matcher collectMatcher = collectPattern.matcher(message);
+					if(collectMatcher.matches()) {
+						GeneralTask task =  new GeneralTask();
+						
+						task.setSelfQnum(selfQnum);
+						task.setType(qmessage.getMessageType());
+						task.setTargetId(qmessage.getGroupId());
+						CfgResource collect = cfgResourceMapper.selectRandomCollectByKey("collect"+"_"+qmessage.getMessageType()+"_"+qmessage.getGroupId()+"_"+qmessage.getUserId());
+						if(collect==null) {
+							task.setMessage(CQUtil.at(qmessage.getUserId())+" 您还未收藏过图片喵~");
+						}else {
+							task.setMessage(CQUtil.imageUrl(collect.getCfgResource()));
+						}
+						return task;
 					}
 //					locked = false;
 					return null;
@@ -71,6 +111,24 @@ public class SetuCmd implements CmdAdapter {
 //		}
 		
 		
+	}
+
+	private static final String COLLECT_PTRN = "收藏";
+	@Override
+	public boolean listen(Qmessage originQmessage, Qmessage qmessage, String selfQnum) {
+		if(originQmessage!=qmessage&&originQmessage.getGroupId().equals(qmessage.getGroupId())&&originQmessage.getUserId().equals(qmessage.getUserId())) {
+			if(COLLECT_PTRN.equals(qmessage.getMessage().trim())&&waitingCollect.containsKey(qmessage.getMessageType()+"_"+qmessage.getGroupId()+"_"+qmessage.getUserId())) {
+				String imgResource = waitingCollect.get(qmessage.getMessageType()+"_"+qmessage.getGroupId()+"_"+qmessage.getUserId());
+				CfgResource rsc = new CfgResource();
+				rsc.setCfgKey("collect"+"_"+qmessage.getMessageType()+"_"+qmessage.getGroupId()+"_"+qmessage.getUserId());
+				rsc.setCfgResource(imgResource);
+				rsc.setResourceType("image");
+				cfgResourceMapper.insert(rsc);
+				taskManager.addGeneralTaskQuick(selfQnum, qmessage.getMessageType(), qmessage.getGroupId(), CQUtil.at(qmessage.getUserId())+" 收藏成功喵，发送[随机收藏]就有机会随机到这张图喵！");
+			}
+			return true;
+		}
+		return false;
 	}
 
 }
