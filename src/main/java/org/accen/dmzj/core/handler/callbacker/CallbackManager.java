36a77b453452ea36ad38845765bf40fba782bcaa
Accen <1339liu@gmail.com>
2019-11-15 09:24:47 +0800

    重写回调的处理，修复同一功能无法同时间多次回调的问题

--- src/main/java/org/accen/dmzj/core/handler/callbacker/CallbackManager.java
+++ src/main/java/org/accen/dmzj/core/handler/callbacker/CallbackManager.java
@@ -1,8 +1,13 @@
 package org.accen.dmzj.core.handler.callbacker;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.accen.dmzj.web.vo.Qmessage;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,18 +18,29 @@ public class CallbackManager {
 
 	@Value("${coolq.bot}")
 	private String botId;
-	private Map<CallbackListener,Qmessage>  listeners = new HashMap<CallbackListener, Qmessage>();
+	private Map<CallbackListener,List<Qmessage>>  listeners = new ConcurrentHashMap<CallbackListener, List<Qmessage>>();
 	
 	public void accept(Qmessage qmessage) {
-		Iterator<CallbackListener> listenerKeys = listeners.keySet().iterator();
-		while(listenerKeys.hasNext()) {
-			CallbackListener listener = listenerKeys.next();
-			if(listener.listen(listeners.get(listener),qmessage,botId)) {
-				listenerKeys.remove();
+		Iterator<Entry<CallbackListener,List<Qmessage>>> listenerEntries = listeners.entrySet().iterator();
+		while(listenerEntries.hasNext()) {
+			Entry<CallbackListener,List<Qmessage>> listenerEntry = listenerEntries.next();
+			if(listenerEntry.getValue()==null||listenerEntry.getValue().isEmpty()) {
+				listenerEntries.remove();
+			}else {
+				Iterator<Qmessage> oqmIte = listenerEntry.getValue().iterator();
+				while(oqmIte.hasNext()) {
+					if(listenerEntry.getKey().listen(oqmIte.next(), qmessage, botId)) {
+						oqmIte.remove();
+					}
+				}
 			}
+			
 		}
 	}
 	public void addCallbackListener(CallbackListener listener,Qmessage orginQmessage) {
-		listeners.put(listener,orginQmessage);
+		if(!listeners.containsKey(listener)) {
+			listeners.put(listener, new LinkedList<Qmessage>());
+		}
+		listeners.get(listener).add(orginQmessage);
 	}
 }
