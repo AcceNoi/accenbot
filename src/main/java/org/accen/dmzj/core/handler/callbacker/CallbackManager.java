6dd532591f86c84909e5bfbf842a37ade254247e
Accen <1339liu@gmail.com>
2019-11-18 12:03:25 +0800

    新增常驻listener，

--- src/main/java/org/accen/dmzj/core/handler/callbacker/CallbackManager.java
+++ src/main/java/org/accen/dmzj/core/handler/callbacker/CallbackManager.java
@@ -1,10 +1,12 @@
 package org.accen.dmzj.core.handler.callbacker;
 
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.accen.dmzj.web.vo.Qmessage;
@@ -17,8 +19,19 @@ public class CallbackManager {
 	@Value("${coolq.bot}")
 	private String botId;
 	private Map<CallbackListener,List<Qmessage>>  listeners = new ConcurrentHashMap<CallbackListener, List<Qmessage>>();
+	/**
+	 * 常驻的listener,通常由listener自己保存上下文，且不通过callbackManager管理其生命周期。
+	 * 例如找图，只会找一次，是可以由callbackManager保存生命周期的，但诸如收藏是由自己管理的（使用cacheMap管理生命周期）,那么它就应该是常驻listener
+	 */
+	private Set<CallbackListener> residentListeners = new HashSet<CallbackListener>(4);
 	
 	public void accept(Qmessage qmessage) {
+		//1.常驻
+		Iterator<CallbackListener> residentListenerIte = residentListeners.iterator();
+		while(residentListenerIte.hasNext()) {
+			residentListenerIte.next().listen(null, qmessage, botId);
+		}
+		//2.一般
 		Iterator<Entry<CallbackListener,List<Qmessage>>> listenerEntries = listeners.entrySet().iterator();
 		while(listenerEntries.hasNext()) {
 			Entry<CallbackListener,List<Qmessage>> listenerEntry = listenerEntries.next();
@@ -40,5 +53,8 @@ public class CallbackManager {
 			listeners.put(listener, new LinkedList<Qmessage>());
 		}
 		listeners.get(listener).add(orginQmessage);
+	}
+	public void addResidentListener(CallbackListener listener) {
+		residentListeners.add(listener);
 	}
 }
