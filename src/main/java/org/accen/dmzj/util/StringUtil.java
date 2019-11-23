78cf3c289a1f1ed594db55d0a0188722c03b5082
Accen <1339liu@gmail.com>
2019-11-24 00:42:55 +0800

    新增unicode的解码

--- src/main/java/org/accen/dmzj/util/StringUtil.java
+++ src/main/java/org/accen/dmzj/util/StringUtil.java
@@ -8,6 +8,7 @@ import java.io.InputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.UUID;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.springframework.util.StringUtils;
@@ -107,5 +108,24 @@ public class StringUtil {
 			}
 		}
 		
+	}
+	
+	/**
+	 * unicode转utf-8的方法
+	 * @param str
+	 * @return
+	 */
+	public static String unicodeToString(String str) {
+		 
+		Pattern pattern = Pattern.compile("(\\\\u(\\p{XDigit}{4}))");
+		Matcher matcher = pattern.matcher(str);
+		char ch;
+		while (matcher.find()) {
+			String group = matcher.group(2);
+			ch = (char) Integer.parseInt(group, 16);
+			String group1 = matcher.group(1);
+			str = str.replace(group1, ch + "");
+		}
+		return str;
 	}
 }
