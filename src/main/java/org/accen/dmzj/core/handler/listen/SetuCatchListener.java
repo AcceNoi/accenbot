a2fc4bf0ae9506991b864c88ea173ea4ae1c2691
Accen <1339liu@gmail.com>
2020-10-19 23:38:26 +0800

    重构了setu监听获取程序，使用greper

--- src/main/java/org/accen/dmzj/core/handler/listen/SetuCatchListener.java
+++ src/main/java/org/accen/dmzj/core/handler/listen/SetuCatchListener.java
@@ -2,13 +2,15 @@ package org.accen.dmzj.core.handler.listen;
 
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.accen.dmzj.core.task.GeneralTask;
 import org.accen.dmzj.core.task.TaskManager;
 import org.accen.dmzj.util.CQUtil;
-import org.accen.dmzj.util.FilePersistentUtil;
-import org.accen.dmzj.util.SetuCatcher;
+import org.accen.dmzj.util.setu.CqSetuGreper;
+import org.accen.dmzj.util.setu.PixivSetuGreper;
+import org.accen.dmzj.util.setu.SetuGreper;
 import org.accen.dmzj.web.vo.Qmessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -20,29 +22,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class SetuCatchListener implements ListenAdpter{
 	private Set<String> allowedSenders = Set.of("2735919291");//yome喵
-	private long minImageSize = 300*1024;//单位为Byte
+	private long minImageSize = 100*1024;//单位为Byte
 	private long maxImageSize = 10*1024*1024;
-	@Autowired
-	private FilePersistentUtil filePersistentUtil;
-	@Autowired
-	private SetuCatcher setuCatcher;
 	@Autowired
 	private TaskManager taskManager;
 	@Override
 	public List<GeneralTask> listen(Qmessage qmessage, String selfQnum) {
-		if(isSetu(qmessage)) {
-			List<String> cqs = CQUtil.grepImageCq(qmessage.getMessage());
-			int successCont= 0;
-			for(String cq:cqs) {
-				String[] info = filePersistentUtil.getImageMetaInfo(cq);
-				long size = Long.parseLong(info[3]);
-				if(size>=minImageSize&&size<=maxImageSize) {
-					if(setuCatcher.catchFromCqImage(cq, setuCatcher.uuSetuName(qmessage))) {
-						successCont++;
-					}
-				}
-			}
-			//通知
+		SetuGreper greper = isSetu(qmessage);
+		if(greper!=null) {
+			int successCont = greper.grep();
+			//通知  
 			if(successCont>0) {
 				taskManager.addGeneralTaskQuick(selfQnum, qmessage.getMessageType(), qmessage.getGroupId(), "涩图充能+"+successCont+"！");
 			}
@@ -55,27 +44,27 @@ public class SetuCatchListener implements ListenAdpter{
 	 * @param qmessage
 	 * @return
 	 */
-	protected boolean isSetu(Qmessage qmessage) {
+	protected SetuGreper isSetu(Qmessage qmessage) {
 		/*if(!"857083789".equals(qmessage.getGroupId())){
 			return false;
 		}*/
 		if(!CQUtil.hasImg(qmessage.getMessage())) {
-			return false;
+			return null;
 		}
 		if(!allowedSenders.contains(qmessage.getUserId())&&!Pattern.matches("^(涩|色|瑟|se)图充能.*", qmessage.getMessage())) {
-			return false;
+			return null;
+		}
+		Matcher mt = pixivPattern.matcher(qmessage.getMessage());
+		if(mt.matches()) {
+			String pidFmting = mt.group(2);
+			return new PixivSetuGreper(pidFmting.split(",|，|、"));
 		}
 		List<String> cqs = CQUtil.grepImageCq(qmessage.getMessage());
-		if(cqs.isEmpty()) {
-			return false;
+		if(!cqs.isEmpty()) {
+			return new CqSetuGreper(minImageSize, maxImageSize, qmessage, cqs);
 		}
-		for(String cq:cqs) {
-			String[] info = filePersistentUtil.getImageMetaInfo(cq);
-			long size = Long.parseLong(info[3]);
-			if(size>=minImageSize&&size<=maxImageSize) {
-				return true;
-			}
-		}
-		return false;
+		return null;
 	}
+	
+	private static final Pattern pixivPattern = Pattern.compile("^(涩|色|瑟|se)图充能(\\d+?((，|,|、)\\d+?)*)");
 }
--- src/main/java/org/accen/dmzj/core/task/api/PixivicApiClient.java
+++ src/main/java/org/accen/dmzj/core/task/api/PixivicApiClient.java
@@ -4,10 +4,12 @@ import java.util.Map;
 
 import org.accen.dmzj.core.annotation.FeignApi;
 
+import feign.Headers;
 import feign.Param;
 import feign.RequestLine;
 
 @FeignApi(host = "https://api.pixivic.com")
+@Headers({"Authorization:eyJhbGciOiJIUzUxMiJ9.eyJwZXJtaXNzaW9uTGV2ZWwiOjEsInJlZnJlc2hDb3VudCI6MSwiaXNCYW4iOjEsInVzZXJJZCI6NDM4OTg4LCJpYXQiOjE2MDIyMTQ4OTQsImV4cCI6MTYwMzk0Mjg5NH0.VnPcn3cdnTKT38CqjW9ELxp8ttSSKuwFby93Mgyvds3cAudL5EhnJ_4vYB8N_40Il9FLQn2EN-ZQTrZ9wc2XQQ"})
 public interface PixivicApiClient {
 	@RequestLine("GET /illustrations?keyword={keyword}&page={page}&pageSize=30&illustType=illust&searchType=original&maxSanityLevel=4")
 	public Map<String,Object> search(@Param("keyword")String keyword,@Param("page")int page);
