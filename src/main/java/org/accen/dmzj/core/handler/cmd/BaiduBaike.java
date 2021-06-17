fdf571455f3ee5b1e815380e04c024eb40c19de4
Accen <1339liu@gmail.com>
2021-06-17 15:42:50 +0800

    使用accenbot框架改写baike

--- src/main/java/org/accen/dmzj/core/handler/cmd/BaiduBaike.java
+++ src/main/java/org/accen/dmzj/core/handler/cmd/BaiduBaike.java
@@ -1,19 +1,20 @@
 package org.accen.dmzj.core.handler.cmd;
 
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
+import org.accen.dmzj.core.annotation.AutowiredRegular;
+import org.accen.dmzj.core.annotation.CmdMessage;
 import org.accen.dmzj.core.annotation.FuncSwitch;
-import org.accen.dmzj.core.task.GeneralTask;
+import org.accen.dmzj.core.annotation.GeneralMessage;
+import org.accen.dmzj.core.annotation.MessageRegular;
 import org.accen.dmzj.core.api.baidu.BaikeApicClientPk;
 import org.accen.dmzj.core.api.vo.BaikeResult;
 import org.accen.dmzj.core.handler.group.Pedia;
+import org.accen.dmzj.core.meta.MessageType;
 import org.accen.dmzj.util.CQUtil;
 import org.accen.dmzj.util.RandomUtil;
 import org.accen.dmzj.web.dao.CmdWikiMapper;
 import org.accen.dmzj.web.vo.CmdWiki;
-import org.accen.dmzj.web.vo.Qmessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -24,7 +25,7 @@ import org.springframework.stereotype.Component;
 			order = 1,
 			groupClass = Pedia.class)
 @Component
-public class BaiduBaike implements CmdAdapter{
+public class BaiduBaike {
 
 	
 	@Autowired
@@ -32,45 +33,33 @@ public class BaiduBaike implements CmdAdapter{
 	@Autowired
 	private CmdWikiMapper cmdWikiMapper;
 	
-	private final static Pattern pattern = Pattern.compile("^了解(.+)");
-	
-	@Override
-	public GeneralTask cmdAdapt(Qmessage qmessage, String selfQnum) {
-		String message = qmessage.getMessage().trim();
-		Matcher matcher = pattern.matcher(message);
-		if(matcher.matches()) {
-			GeneralTask task = new GeneralTask();
-			task.setSelfQnum(selfQnum);
-			task.setType(qmessage.getMessageType());
-			task.setTargetId(qmessage.getGroupId());
-			
-			String kw = matcher.group(1).trim();
-			//1.先在wiki中查
-			CmdWiki wiki = cmdWikiMapper.selectByName(kw);
-			if(wiki==null) {
-				//名字没匹配到，再去找关键字
-				List<CmdWiki> wikis = cmdWikiMapper.findByKeyword(kw);
-				if(wikis!=null&&!wikis.isEmpty()) {
-					wiki = wikis.get(RandomUtil.randomInt(wikis.size()));
-				}
+	@CmdMessage(messageType = MessageType.GROUP)
+	@MessageRegular(expression = "^了解(.+)")
+	@GeneralMessage
+	public String cmdAdapt(@AutowiredRegular(1) String kw) {
+		
+		//1.先在wiki中查
+		CmdWiki wiki = cmdWikiMapper.selectByName(kw);
+		if(wiki==null) {
+			//名字没匹配到，再去找关键字
+			List<CmdWiki> wikis = cmdWikiMapper.findByKeyword(kw);
+			if(wikis!=null&&!wikis.isEmpty()) {
+				wiki = wikis.get(RandomUtil.randomInt(wikis.size()));
 			}
-			if(wiki==null) {
-				//2.找不到则去百度百科查
-				BaikeResult br = baikeApicClientPk.baike(kw);
-				if(br!=null) {
-					task.setMessage(CQUtil.imageUrl(br.getImageUrl())+br.getTitle()+"\n"+br.getSummary()+"["+br.getUrl()+"]喵~");
-				}else {
-					task.setMessage("抱歉，我太弱了，找不到该词条喵~");
-				}
-			}else {
-				
-				task.setMessage((wiki.getImage()==null?"":CQUtil.imageUrl(RandomUtil.randomStringSplit(wiki.getImage())))
-						+wiki.getWikiName()+"\n"+wiki.getContent());
-			}
-			
-			return task;
 		}
-		return null;
+		if(wiki==null) {
+			//2.找不到则去百度百科查
+			BaikeResult br = baikeApicClientPk.baike(kw);
+			if(br!=null) {
+				return CQUtil.imageUrl(br.getImageUrl())+br.getTitle()+"\n"+br.getSummary()+"["+br.getUrl()+"]喵~";
+			}else {
+				return "抱歉，我太弱了，找不到该词条喵~";
+			}
+		}else {
+			
+			return (wiki.getImage()==null?"":CQUtil.imageUrl(RandomUtil.randomStringSplit(wiki.getImage())))
+					+wiki.getWikiName()+"\n"+wiki.getContent();
+		}
 	}
 	
 }
