bf2e807e2367633b347d6362c0516d10d76d52ee
Accen <1339liu@gmail.com>
2021-06-18 11:28:25 +0800

    feat 新增框架metadata

--- src/main/java/org/accen/dmzj/web/controller/CmdController.java
+++ src/main/java/org/accen/dmzj/web/controller/CmdController.java
@@ -1,5 +1,6 @@
 package org.accen.dmzj.web.controller;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -14,11 +15,14 @@ import org.accen.dmzj.core.meta.PostType;
 import org.accen.dmzj.util.ApproximatelyEqualsUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * 
@@ -26,7 +30,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  *
  */
 @Controller
-@RequestMapping("/cmd")
+@RequestMapping("/monitor")
 public class CmdController implements ContextPostProcessor,ProxyPostProcessor{
 	private Map<String, AccenbotContext> copy = new HashMap<String, AccenbotContext>(4);
 	private Map<AccenbotContext,List<AccenbotCmdProxy>> proxyCopies = new HashMap<>(4);
@@ -40,10 +44,36 @@ public class CmdController implements ContextPostProcessor,ProxyPostProcessor{
 		proxyCopies.get(context).add(proxy);
 	}
 	public CmdController(@Autowired @Qualifier("accenbotContext") AccenbotContext accenbotContext) {
-		
+		initMetadata();
 	}
 	
-	@GetMapping("/list/{scope}")
+	private Map<String, Object> accenbotMetaInfo;
+	private final static String ACCENBOT_METADATA_LOCATION = "META-INF/accenbot-metadata.json";
+	@SuppressWarnings("unchecked")
+	private void initMetadata() {
+		try {
+			accenbotMetaInfo = new ObjectMapper().readValue(new ClassPathResource(ACCENBOT_METADATA_LOCATION).getInputStream(), Map.class);
+			List<Map<String, Object>> apis = List.of(
+					Map.of("url","/monitor/cmd/list/","description","all cmd list."),
+					Map.of("url","/monitor/cmd/list/{scope}","description","just see the cmd list filt by scope like messsage, meta, request or notice.")
+					);
+			accenbotMetaInfo.put("monitors", apis);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	@GetMapping("/help")
+	@ResponseBody
+	public Map<String,Object> help(){
+		return accenbotMetaInfo;
+	}
+	
+	@GetMapping("/")
+	public String home(){
+		return "redirect:/monitor/help";
+	}
+	
+	@GetMapping("/cmd/list/{scope}")
 	@ResponseBody
 	public Map<String,Object> list(@PathVariable("scope")String scope) {
 		Map<String,Object> format = new HashMap<String, Object>();
@@ -65,9 +95,13 @@ public class CmdController implements ContextPostProcessor,ProxyPostProcessor{
 					});
 		return format;
 	}
-	@GetMapping("/list")
+	@GetMapping("/cmd/list")
 	@ResponseBody
 	public Map<String,Object> list(){
 		return list(null);
+	}
+	@GetMapping("/detail/{aa}")
+	public Map<String,Object> detail(){
+		return null;
 	}
 }
